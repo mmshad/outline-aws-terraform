b356a251971701822458eb3dd98ddba9b417e7e0
Mahmood Shad <31811010+mmshad@users.noreply.github.com>
2023-03-06 21:03:47 -0500

    added EC2 count for multiple VPN servers

--- main.tf
+++ main.tf
@@ -81,6 +81,7 @@ module "security_group" {
 }
 
 resource "aws_instance" "outline_server" {
+  count         = 1
   ami           = var.instance_ami
   instance_type = var.instance_type
   key_name      = aws_key_pair.outline_key_pair.key_name
